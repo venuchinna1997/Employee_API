9ff0cf1f52aa0c2daf46e3bd754077ed24308455
vgrgala@gmail.com <Chinna@1025>
2023-06-23 15:40:58 -0500

    Added code for getting employee with id and updating employee API

--- src/main/java/com/example/Employee/controller/EmployeeController.java
+++ src/main/java/com/example/Employee/controller/EmployeeController.java
@@ -1,7 +1,9 @@
 package com.example.Employee.controller;
 
 import com.example.Employee.entity.Employee;
+import com.example.Employee.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.example.Employee.service.EmployeeService;
 
@@ -22,5 +24,16 @@ public class EmployeeController {
     @PostMapping("/")
     public Employee saveEmployee(@RequestBody Employee employee){
         return employeeService.saveEmployee(employee);
+    }
+
+    @GetMapping("/employee/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long id){
+               return employeeService.getEmployeeById(id);
+    }
+
+    @PutMapping("/employee/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable("id") Long id, @RequestBody Employee employeeDetails){
+        return employeeService.updateEmployee(id,employeeDetails);
+
     }
 }
--- src/main/java/com/example/Employee/service/EmployeeService.java
+++ src/main/java/com/example/Employee/service/EmployeeService.java
@@ -1,7 +1,9 @@
 package com.example.Employee.service;
 
 import com.example.Employee.entity.Employee;
+import com.example.Employee.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import com.example.Employee.repository.EmployeeRepository;
 
@@ -19,6 +21,21 @@ public class EmployeeService {
 
     public Employee saveEmployee(Employee employee) {
         return employeeRepository.save(employee);
+    }
+
+    public ResponseEntity<Employee> getEmployeeById(Long id){
+
+        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id:"+id));
+        return ResponseEntity.ok(employee);
+    }
+
+    public ResponseEntity<Employee> updateEmployee(Long id, Employee employeeDetails){
+        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id:"+id));
+        employee.setName(employeeDetails.getName());
+        employee.setEmail(employeeDetails.getEmail());
+        employee.setAge(employeeDetails.getAge());
+        employeeRepository.save(employee);
+        return ResponseEntity.ok(employee);
     }
 }
 
